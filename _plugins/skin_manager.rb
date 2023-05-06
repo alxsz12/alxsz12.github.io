f67fa748debb607f6cfe935127b153bbd2310d04
alxsz12 <alexseitz12@gmail.com>
2023-05-06 14:23:51 -0500

    Update skin_manager.rb

--- _plugins/skin_manager.rb
+++ _plugins/skin_manager.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
-module Minima
+module MinimaDemo
   class << self
     attr_accessor :available_skins
   end
 
   Jekyll::Hooks.register :site, :post_read do |site|
     skins_dir = site.in_theme_dir("_sass", "minima", "skins")
-    Minima.available_skins = Dir["#{skins_dir}/*.scss"].map { |i| File.basename(i, ".scss") }
+    MinimaDemo.available_skins = Dir["#{skins_dir}/*.scss"].map { |i| File.basename(i, ".scss") }
   end
 
   Jekyll::Hooks.register [:pages, :documents], :pre_render do |doc, payload|
-    payload["page"]["available_skins"] = Minima.available_skins
+    payload["page"]["available_skins"] = MinimaDemo.available_skins
   end
 
   class SkinPage < Jekyll::PageWithoutAFile
@@ -22,10 +22,10 @@ module Minima
 
   class StyleSheetGenerator < Jekyll::Generator
     def generate(site)
-      Minima.available_skins.each do |skin_name|
+      MinimaDemo.available_skins.each do |skin_name|
         site.pages << SkinPage.new(site, skin_name).tap do |page|
           page.data["skin_name"] = skin_name
-          page.content = <<~ SCSS
+          page.content = <<~SCSS
             @import
               "minima/skins/{{ page.skin_name }}",
               "minima/initialize";
